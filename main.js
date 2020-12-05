f4a4415bb919bb2081405e02f75cb5e66258f0d2
Bill Ni <bill.8ni@gmail.com>
2020-12-04 19:18:18 -0500

    Update main.js

--- main.js
+++ main.js
@@ -13,16 +13,12 @@ function rept() {
     var minkeys = 5, maxkeys = 8;
     var numkeys = Math.floor(Math.random() * (minkeys - maxkeys + 1) + minkeys);
     setTimeout(function(){do_riff(numkeys)}, rand * 100);
-  }
-
-  if(riff >= 0.8){
-    min = 23,max = 36;
   }else{
     min = 7,max = 20;
+    rand = Math.floor(Math.random() * (max - min + 1) + min);
+    window.dispatchEvent(new KeyboardEvent('keydown',{'key':'a'}));
+    setTimeout(rept, rand * 100);
   }
-  rand = Math.floor(Math.random() * (max - min + 1) + min);
-  window.dispatchEvent(new KeyboardEvent('keydown',{'key':'a'}));
-  setTimeout(rept, rand * 100);
 }
 
 function do_riff(t){
@@ -31,6 +27,10 @@ function do_riff(t){
     var min = 2,max = 5;
     var rand = Math.floor(Math.random() * (max - min + 1) + min);
     setTimeout(function(){do_riff(t-1)}, rand * 100);
+  }else{
+    var min = 2,max = 5;
+    var rand = Math.floor(Math.random() * (max - min + 1) + min);
+    setTimeout(rept, rand * 100);
   }
 }
 
